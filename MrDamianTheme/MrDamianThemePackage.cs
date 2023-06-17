d683658cc51b429bdfdf4a2e2e5f90b74cdb08a5
MrDamian <info@it-tips.com>
2023-06-17 22:14:45 +0300

    Add project files.

---
+++ MrDamianTheme/MrDamianThemePackage.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+namespace MrDamianTheme
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid(MrDamianThemePackage.PackageGuidString)]
+    public sealed class MrDamianThemePackage : AsyncPackage
+    {
+        public const string PackageGuidString = "521df2c8-0562-43bf-95d4-8532c1c531fe";
+        #region Package Members
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+        }
+        #endregion
+    }
+}
